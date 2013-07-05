4f795f4e13df4ec9655655b303c0914c06ffb38f
Gabriele de Capoa <gabriele.decapoa@gmail.com>
2013-07-05 18:04:18 +0200

    Update of TwitterSpout and ExpanderBolt

---
+++ RTwUP/src/main/java/storm/ExpanderBolt.java
@@ -0,0 +1,56 @@
+package storm;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+public class ExpanderBolt extends BaseBasicBolt {
+
+	private static final long serialVersionUID = 1L;
+
+	public void execute(Tuple input, BasicOutputCollector collector) {
+		String url = (String) input.getValue(0);
+		try {
+			URL url_da_verificare;
+			boolean flag = false;
+			do {
+				url_da_verificare = new URL(url);
+				HttpURLConnection connection = (HttpURLConnection) url_da_verificare
+						.openConnection();
+				connection.setInstanceFollowRedirects(true);
+				byte[] expanded = new byte[256];
+				connection.getInputStream().read(expanded, 0, 256);
+				String urlExpanded = new String(expanded);
+				if (urlExpanded.contains("href")) {
+					urlExpanded = urlExpanded.substring(urlExpanded
+							.indexOf("href") + 6);
+					urlExpanded = urlExpanded.substring(0,
+							urlExpanded.indexOf("\""));
+					url_da_verificare = new URL(urlExpanded);
+				} else {
+					flag = true;
+				}
+			} while (!flag);
+			collector.emit(new Values(url_da_verificare.getHost()));
+		} catch (MalformedURLException e) {
+
+		} catch (IOException e) {
+
+		}
+	}
+
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("expanded_url"));
+		
+	}
+	
+
+}
--- RTwUP/src/main/java/storm/TwitterSpout.java
+++ RTwUP/src/main/java/storm/TwitterSpout.java
@@ -3,12 +3,14 @@ package storm;
 import java.util.Map;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import twitter4j.FilterQuery;
 import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
+import twitter4j.URLEntity;
 import twitter4j.conf.ConfigurationBuilder;
 import backtype.storm.spout.SpoutOutputCollector;
 import backtype.storm.task.TopologyContext;
@@ -18,18 +20,18 @@ import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
 
 public class TwitterSpout extends BaseRichSpout {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private LinkedBlockingQueue<Status> queue = null;
 	private SpoutOutputCollector collector;
-	
+
 	public void open(Map conf, TopologyContext context,
 			SpoutOutputCollector collector) {
-		
-		this.queue= new LinkedBlockingQueue<Status>();
-		this.collector=collector;
-		
+
+		this.queue = new LinkedBlockingQueue<Status>();
+		this.collector = collector;
+
 		ConfigurationBuilder cb = new ConfigurationBuilder();
 		cb.setDebugEnabled(true)
 				.setOAuthConsumerKey("P9c5PqNZ2HvANU6B8Rrp1A")
@@ -39,12 +41,12 @@ public class TwitterSpout extends BaseRichSpout {
 						"1546231212-TKDS2JM9sBp351uEuvnbn1VSPLR5mUKhZxwmfLr")
 				.setOAuthAccessTokenSecret(
 						"8krjiVUEAoLvFrLC8ryw8iaU2PKTU80WHZaWevKGk2Y");
-		
-		TwitterStream ts = new TwitterStreamFactory(cb.build()).getInstance();
-		
-		StatusListener listener = new StatusListener(){
 
-			public void onException(Exception arg0)  {
+		TwitterStream ts = new TwitterStreamFactory(cb.build()).getInstance();
+
+		StatusListener listener = new StatusListener() {
+
+			public void onException(Exception arg0) {
 			}
 
 			public void onDeletionNotice(StatusDeletionNotice arg0) {
@@ -62,21 +64,27 @@ public class TwitterSpout extends BaseRichSpout {
 
 			public void onTrackLimitationNotice(int arg0) {
 			}
-			
+
 		};
-		
+
 		ts.addListener(listener);
-		ts.links(10);
+		FilterQuery query = new FilterQuery();
+		double[][] bbox = { { 12.38, 41.80 }, { 12.60, 42.00 } };
+		query.locations(bbox);
+		ts.filter(query);
 	}
 
 	public void nextTuple() {
 		try {
 			Status retrieve = queue.take();
-			this.collector.emit(new Values(retrieve));
+			URLEntity[] urls = retrieve.getURLEntities();
+			if (urls != null) {
+				for (URLEntity url : urls)
+				this.collector.emit(new Values(url.getURL()));
+			}
 		} catch (InterruptedException e) {
 			System.err.println("ERRORE SULLO SPOUT: " + e.getMessage());
 		}
-		
 
 	}
 
