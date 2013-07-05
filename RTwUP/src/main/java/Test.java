982d014b728f6a1ee883fedca40fafc72aa1cbcb
Dani7B <dan.morgantini@stud.uniroma3.it>
2013-07-05 13:26:20 +0200

    Switched to Twitter Stream API (check README). Test app now retrieve tweets from Rome urban area

--- RTwUP/src/main/java/Test.java
+++ RTwUP/src/main/java/Test.java
@@ -1,63 +1,54 @@
-import twitter4j.Location;
-import twitter4j.AccountSettings;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
+import twitter4j.FilterQuery;
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.auth.AccessToken;
 
 
 public class Test {
 	
 	public static void main(String[]args) {
-
-		try {
-			ConfigurationBuilder cb = new ConfigurationBuilder();
-			cb.setDebugEnabled(true)
-			  .setOAuthConsumerKey("P9c5PqNZ2HvANU6B8Rrp1A")
-			  .setOAuthConsumerSecret("iKUCqCYbvI8Tam7zGgIRiO6Zcyh2hw7Nm0v97lE")
-			  .setOAuthAccessToken("1546231212-TKDS2JM9sBp351uEuvnbn1VSPLR5mUKhZxwmfLr")
-			  .setOAuthAccessTokenSecret("8krjiVUEAoLvFrLC8ryw8iaU2PKTU80WHZaWevKGk2Y");
-			TwitterFactory tf = new TwitterFactory(cb.build());
-			Twitter twitter = tf.getInstance();
+			  
+			TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+			twitterStream.setOAuthConsumer("P9c5PqNZ2HvANU6B8Rrp1A", "iKUCqCYbvI8Tam7zGgIRiO6Zcyh2hw7Nm0v97lE");
+			AccessToken accessToken = new AccessToken("1546231212-TKDS2JM9sBp351uEuvnbn1VSPLR5mUKhZxwmfLr","8krjiVUEAoLvFrLC8ryw8iaU2PKTU80WHZaWevKGk2Y");
+			twitterStream.setOAuthAccessToken(accessToken);
 			
-            AccountSettings settings = twitter.getAccountSettings();
-            System.out.println("Sleep time enabled: " + settings.isSleepTimeEnabled());
-            System.out.println("Sleep end time: " + settings.getSleepEndTime());
-            System.out.println("Sleep start time: " + settings.getSleepStartTime());
-            System.out.println("Geo enabled: " + settings.isGeoEnabled());
-            System.out.println("Listing trend locations:");
-            Location[] locations = settings.getTrendLocations();
-            if(locations.length == 0)
-            	System.out.println("No trend locations yet");
-            else {
-	            for (Location location : locations) {
-	                System.out.println(" " + location.getName());
+			StatusListener listener = new StatusListener() {
+	            public void onStatus(Status status) {
+	                System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
 	            }
-            }
-            System.exit(0);
-        } catch (TwitterException te) {
-            te.printStackTrace();
-            System.out.println("Failed to get account settings: " + te.getMessage());
-            System.exit(-1);
-        }
+
+	            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+	                System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+	            }
+
+	            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+	                System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+	            }
+
+	            public void onScrubGeo(long userId, long upToStatusId) {
+	                System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+	            }
+
+	            public void onStallWarning(StallWarning warning) {
+	            	System.out.println("Got stall warning:" + warning);
+	            }
+
+	            public void onException(Exception ex) {
+	                ex.printStackTrace();
+	            }
+	        };
+	        
+	        twitterStream.addListener(listener);
+	        FilterQuery query = new FilterQuery();
+			double[][] bbox = {{12.38,41.80}, {12.60, 42.00}};
+			query.locations(bbox);
+	        twitterStream.filter(query);
 		
-		/*
-		ConfigurationBuilder cb = new ConfigurationBuilder();
-		cb.setDebugEnabled(true)
-		  .setOAuthConsumerKey("*********************")
-		  .setOAuthConsumerSecret("******************************************")
-		  .setOAuthAccessToken("**************************************************")
-		  .setOAuthAccessTokenSecret("******************************************");
-		TwitterFactory tf = new TwitterFactory(cb.build());
-		Twitter twitter = tf.getInstance();
-		
-	    List<Status> statuses = twitter.getHomeTimeline();
-	    
-	    
-	    for (Status status : statuses) {
-	        System.out.println(status.getUser().getName() + ":" +
-	                           status.getText());
-	    }*/
 	}
     
 }
