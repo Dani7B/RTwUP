03592b570870ef811ca378966ccc401f9dfa2669
Gabriele de Capoa <gabriele.decapoa@gmail.com>
2013-07-06 16:19:52 +0200

    Merge with RTwUP core

--- RTwUP/src/main/java/storm/URLCounterBolt.java
+++ RTwUP/src/main/java/storm/URLCounterBolt.java
@@ -11,9 +11,9 @@ import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 
 /**
- * This bolt count the URL.
+ * This bolt counts the URL.
  * 
- * @author Gabriele de Capoa
+ * @author Gabriele de Capoa, Gabriele Proni
  *
  */
 
