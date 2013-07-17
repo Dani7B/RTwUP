ebec2718fab44e6b3cdd314b9f7cbdea5d93704a
Matteo Moci <mox601@libero.it>
2013-07-17 17:15:17 +0200

    added redis publisher test

---
+++ RTwUP/src/test/java/redis/RedisPublisherTestCase.java
@@ -0,0 +1,39 @@
+package redis;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPubSub;
+
+import static org.testng.Assert.assertTrue;
+
+/**
+ * @author Matteo Moci ( matteo (dot) moci (at) gmail (dot) com )
+ */
+public class RedisPublisherTestCase {
+
+    private Jedis jedis;
+    private RedisStringPublisher redisPublisher;
+
+    @BeforeClass
+    public void setUp() {
+
+        this.jedis = null;
+        //        this.jedis = new Jedis("localhost");
+        this.redisPublisher = new RedisStringPublisher(this.jedis);
+
+        assertTrue(redisIsRunning());
+    }
+
+    private Boolean redisIsRunning() {
+//        String pingResponse = jedis.ping();
+
+        return true;
+    }
+
+    @Test
+    public void shouldPublishAndConsumeOnRedis() {
+
+    }
+
+}
---
+++ RTwUP/src/test/java/redis/RedisStringPublisher.java
@@ -0,0 +1,22 @@
+package redis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPubSub;
+
+/**
+ * @author Matteo Moci ( matteo (dot) moci (at) gmail (dot) com )
+ */
+public class RedisStringPublisher implements StringPublisher {
+
+    private final Jedis jedis;
+
+    public RedisStringPublisher(Jedis jedis) {
+        this.jedis = jedis;
+
+    }
+
+    @Override
+    public void publish(String channel, String message) {
+        Long value = this.jedis.publish(channel, message);
+    }
+}
