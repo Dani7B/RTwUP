b4f5a1694db6b087f6aaf3f41c88345d5f4cb5d0
Dani7B <dan.morgantini@stud.uniroma3.it>
2013-07-22 11:18:46 +0200

    Set jar as packaging

--- RTwUP/src/test/java/redis/RedisPublisherIntegrationTestCase.java
+++ RTwUP/src/test/java/redis/RedisPublisherIntegrationTestCase.java
@@ -61,7 +61,6 @@ public class RedisPublisherIntegrationTestCase {
 					LOGGER.info("Subscription ended.");
 				} catch (Exception e) {
 					LOGGER.error("Subscribing failed.", e);
-					System.out.println("HERE");
 				}
 			}
 		}).start();
